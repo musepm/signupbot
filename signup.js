4617db3e038fa0696852ac4ca69783300b8ca4d1
Jason Livesay <ithkuil@gmail.com>
2015-06-05 19:19:17 +0000

    relative path

--- signup.js
+++ signup.js
@@ -2,5 +2,5 @@ var casper = require('casper').create();
 
 var service = casper.cli.args[0]
 
-require('musepm-'+service+'/lib/signupbot');
+require('./node_modules/musepm-'+service+'/lib/signupbot');
 
